d6b28258d29a8c6432ddd050f0f2a0df85ed43b8
kevin <kpsanpedro@myseneca.ca>
2022-05-26 07:01:56 -0400

    show controlled error from  401 to 500

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,17 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es2021": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": "latest"
-    },
-    "rules": {
-    }
-}
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    jest: true,
+    // jest:true will configure ESLint for Jest, see:
+    // https://eslint.org/docs/user-guide/configuring/language-options#specifying-environments
+    // ESLint will give lots of lint errors when you use
+    // Jest's global functions in your tests). We do this by adding another env setting for jest
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {},
+};
