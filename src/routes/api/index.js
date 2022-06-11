79ca7727a15a16e3ed15223bec3dc538faa4f455
kevin <kpsanpedro@myseneca.ca>
2022-06-11 05:11:36 -0400

    update for get, and post

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -5,12 +5,32 @@
  */
 const express = require('express');
 
+const contentType = require('content-type');
+const { Fragment } = require('../../model/fragment');
+
 // Create a router on which to mount our API endpoints
 const router = express.Router();
 
 // Define our first route, which will be: GET /v1/fragments
 router.get('/fragments', require('./get'));
-
+router.get('/fragments/:id', require('./getById'));
+router.get('/fragments/:id/info', require('./getByIdInfo'));
 // Other routes will go here later on...
+// Support sending various Content-Types on the body up to 5M in size
+const rawBody = () =>
+  express.raw({
+    inflate: true,
+    limit: '5mb', //limit only 5m upload, if more will send an error
+    type: (req) => {
+      //this function will figure out if function is supported
+      // See if we can parse this content type. If we can, `req.body` will be
+      // a Buffer (e.g., `Buffer.isBuffer(req.body) === true`). If not, `req.body`
+      // will be equal to an empty Object `{}` and `Buffer.isBuffer(req.body) === false`
+      const { type } = contentType.parse(req);
+      return Fragment.isSupportedType(type);
+    },
+  });
+// Use a raw body parser for POST, which will give a `Buffer` Object or `{}` at `req.body`
+router.post('/fragments', rawBody(), require('./post'));
 
 module.exports = router;
