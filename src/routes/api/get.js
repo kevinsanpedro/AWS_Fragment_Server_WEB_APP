85665b34b2f968cc9ac0c5299cbf29c302a3a269
kevin <kpsanpedro@myseneca.ca>
2022-06-11 05:36:07 -0400

    removing try catch

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -8,15 +8,11 @@ const logger = require('../../logger');
 module.exports = async (req, res) => {
   //console.log(req.query({ expand: 1 }));
   let isTrue = false;
-  try {
-    if (req.query.expand == 1) isTrue = true;
 
-    const fragments = await Fragment.byUser(req.user, isTrue);
-    res.status(200).send(createSuccessResponse({ fragments }));
+  if (req.query.expand == 1) isTrue = true;
 
-    logger.info({ fragments }, `get fragment`);
-  } catch (err) {
-    logger.fatal({ err, origin }, 'uncaughtException');
-    res.status(404).send(createErrorResponse(404, 'Page not found'));
-  }
+  const fragments = await Fragment.byUser(req.user, isTrue);
+  res.status(200).send(createSuccessResponse({ fragments }));
+
+  logger.info({ fragments }, `get fragment`);
 };
