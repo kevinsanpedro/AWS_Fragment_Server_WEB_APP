11c23f4f148d05919bfc3be0c7967339a8566c35
kevin <kpsanpedro@myseneca.ca>
2022-06-11 05:10:07 -0400

    adding get command

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -1,8 +1,22 @@
 /**
  * Get a list of fragments for the current user
  */
-const { createSuccessResponse } = require('../../response');
-module.exports = (req, res) => {
-  const data = { fragments: [] };
-  res.status(200).send(createSuccessResponse(data));
+const { createSuccessResponse, createErrorResponse } = require('../../response');
+const { Fragment } = require('../../model//fragment');
+const logger = require('../../logger');
+
+module.exports = async (req, res) => {
+  //console.log(req.query({ expand: 1 }));
+  let isTrue = false;
+  try {
+    if (req.query.expand == 1) isTrue = true;
+
+    const fragments = await Fragment.byUser(req.user, isTrue);
+    res.status(200).send(createSuccessResponse({ fragments }));
+
+    logger.info({ fragments }, `get fragment`);
+  } catch (err) {
+    logger.fatal({ err, origin }, 'uncaughtException');
+    res.status(404).send(createErrorResponse(404, 'Page not found'));
+  }
 };
