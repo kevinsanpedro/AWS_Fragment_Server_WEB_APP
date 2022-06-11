70eb8dc07c5d7226f5b1c6046c0236261a6e9675
kevin <kpsanpedro@myseneca.ca>
2022-06-11 05:10:40 -0400

    adding get by id

---
+++ src/routes/api/getById.js
@@ -0,0 +1,34 @@
+/**
+ * Get a list of fragments for the current user
+ */
+const { createErrorResponse } = require('../../response');
+const { Fragment } = require('../../model//fragment');
+
+module.exports = async (req, res) => {
+  let fragment;
+  let result;
+  //console.log(req.params.id);
+  let query = req.params.id.split('.');
+
+  // if (query.length == 1) {
+  try {
+    if (query.length === 2) {
+      fragment = await Fragment.byId(req.user, query[0]);
+      result = await fragment.getData();
+      res.setHeader('Content-Type', `'text/${query[1]}`);
+      result = result.toString();
+      // let parser = new DOMParser();
+      // let floatingElement = parser.parseFromString(result, 'text/html');
+      res.status(200).send(result);
+    } else {
+      fragment = await Fragment.byId(req.user, req.params.id);
+      res.setHeader('Content-Type', fragment.mimeType);
+      result = await fragment.getData();
+      result = result.toString();
+
+      res.status(200).send(result);
+    }
+  } catch (err) {
+    res.send(createErrorResponse(404, 'Page not found'));
+  }
+};
