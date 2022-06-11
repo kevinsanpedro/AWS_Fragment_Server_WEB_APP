1457691c48fcd0cac8373bc74661c19c37b30a3e
kevin <kpsanpedro@myseneca.ca>
2022-06-11 07:33:18 -0400

    adding htppassd

--- src/authorization/index.js
+++ src/authorization/index.js
@@ -3,8 +3,10 @@
 
 // src/authorization/index.js
 // Prefer Amazon Cognito
+
 if (process.env.AWS_COGNITO_POOL_ID && process.env.AWS_COGNITO_CLIENT_ID) {
   module.exports = require('./cognito');
+  module.exports = require('./basic-auth');
 }
 // Also allow for an .htpasswd file to be used, but not in production
 else if (process.env.HTPASSWD_FILE && process.NODE_ENV !== 'production') {
