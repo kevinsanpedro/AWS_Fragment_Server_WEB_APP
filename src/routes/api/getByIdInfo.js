a95e0d4c793857621545dacbe1080d80e7eeecc1
kevin <kpsanpedro@myseneca.ca>
2022-06-26 08:14:09 -0400

    fixed bug

--- src/routes/api/getByIdInfo.js
+++ src/routes/api/getByIdInfo.js
@@ -7,10 +7,9 @@ const logger = require('../../logger');
 
 module.exports = async (req, res) => {
   try {
-    const fragments = await Fragment.byUser(req.user, true);
-    res.status(200).send(createSuccessResponse({ fragments }));
-
-    logger.info({ fragments }, `get fragment`);
+    const fragment = await Fragment.byId(req.user, req.params.id);
+    res.status(200).send(createSuccessResponse({ fragment: fragment }));
+    logger.info(fragment, `get fragment`);
   } catch (err) {
     logger.fatal({ err, origin }, 'uncaughtException');
     res.status(404).send(createErrorResponse(404, 'page not found'));
