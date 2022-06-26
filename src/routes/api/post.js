4fcd67879c63a6f4af2c41cd42eb8be63c82a94b
kevin <kpsanpedro@myseneca.ca>
2022-06-26 08:14:23 -0400

    fixed bug

--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -3,6 +3,8 @@ const { createSuccessResponse, createErrorResponse } = require('../../response')
 const { Fragment } = require('../../model//fragment');
 
 module.exports = async (req, res, next) => {
+  //professors note:
+  // Do a check at the top for Buffer.isBuffer() and deal with the 415 vs. here, since you could have other problems that lead to landing in this catch, and might require a 5xx response instead
   try {
     const fragment = new Fragment({
       ownerId: req.user,
@@ -16,7 +18,7 @@ module.exports = async (req, res, next) => {
     //change process.env.api_url for aws server
 
     res.set('location', `${process.env.API_URL}/v1/fragments/${fragment.id}`);
-    res.status(201).send(createSuccessResponse(fragment));
+    res.status(201).send(createSuccessResponse({ fragment }));
   } catch (err) {
     //check return 415 if  content type if supported send error;
     res.status(415).send(createErrorResponse(415, 'invalid content type'));
