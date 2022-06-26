421ec2e11f7d53fd8744d0d5256924d9c42106d0
kevin <kpsanpedro@myseneca.ca>
2022-06-26 08:12:48 -0400

    update get by id

--- src/routes/api/getById.js
+++ src/routes/api/getById.js
@@ -3,28 +3,40 @@
  */
 const { createErrorResponse } = require('../../response');
 const { Fragment } = require('../../model//fragment');
+const path = require('node:path');
+var MarkdownIt = require('markdown-it'),
+  md = new MarkdownIt();
 
 module.exports = async (req, res) => {
   let fragment;
   let result;
   //console.log(req.params.id);
-  let query = req.params.id.split('.');
+  //let query = req.params.id.split('.');
 
-  // if (query.length == 1) {
+  const convertExt = path.extname(req.params.id); //return .html, .txt
+  const fragmentId = path.basename(req.params.id, convertExt); // return fragmentId
   try {
-    if (query.length === 2) {
-      fragment = await Fragment.byId(req.user, query[0]);
+    //get the fragment
+    fragment = await Fragment.byId(req.user, fragmentId);
+
+    //if no extention
+    if (!convertExt) {
+      //return a buffer
       result = await fragment.getData();
-      res.setHeader('Content-Type', `'text/${query[1]}`);
-      result = result.toString();
-      res.status(200).send(result);
-    } else {
-      fragment = await Fragment.byId(req.user, req.params.id);
-      res.setHeader('Content-Type', fragment.mimeType);
-      result = await fragment.getData();
-      result = result.toString();
+
+      //if the fragment content type is text,
+      // -convert raw buffer to plain text
+      // -if not respawn with raw buffer
+      if (fragment.isText) result = result.toString();
 
       res.status(200).send(result);
+    } else if (convertExt) {
+      //if extension exist
+      //convert current fragment content type to new type
+      //Initial, you only need to support Markdown fragments (.md)
+      //converted to HTML (.html) using markdown-it
+
+      res.send('this option is still not available');
     }
   } catch (err) {
     res.send(createErrorResponse(404, 'Page not found'));
