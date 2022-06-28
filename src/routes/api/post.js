16d2fa9fa92999e21f00a7631d46d603252a3228
kevin <kpsanpedro@myseneca.ca>
2022-06-28 18:37:27 -0400

    add error 500 and 415

--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -2,26 +2,32 @@
 const { createSuccessResponse, createErrorResponse } = require('../../response');
 const { Fragment } = require('../../model//fragment');
 
-module.exports = async (req, res, next) => {
-  //professors note:
-  // Do a check at the top for Buffer.isBuffer() and deal with the 415 vs. here, since you could have other problems that lead to landing in this catch, and might require a 5xx response instead
+module.exports = async (req, res) => {
   try {
+    //check if the obj is empty
+    if (req.body.length === undefined) {
+      throw new Error('not supported');
+    }
+
     const fragment = new Fragment({
       ownerId: req.user,
       type: req.get('Content-type'),
       size: req.body.length,
     });
 
+    //save the fragment
     await fragment.save();
+    //save a raw binary data
     await fragment.setData(req.body);
-    //location header is to locate where we put the fragments in the system
-    //change process.env.api_url for aws server
 
-    res.set('location', `${process.env.API_URL}/v1/fragments/${fragment.id}`);
-    res.status(201).send(createSuccessResponse({ fragment }));
-  } catch (err) {
-    //check return 415 if  content type if supported send error;
-    res.status(415).send(createErrorResponse(415, 'invalid content type'));
-    next(err);
+    //set the location where it will
+    //then send a respawn with fragment meta data
+    res
+      .set('location', `${process.env.API_URL}/v1/fragments/${fragment.id}`)
+      .send(createSuccessResponse({ fragment }));
+  } catch (Error) {
+    if (Error.message === 'not supported')
+      res.status(415).send(createErrorResponse(415, 'Content type is not supported'));
+    else res.status(500).send(createErrorResponse(500, Error));
   }
 };
