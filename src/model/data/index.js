1b6b4a742e631be82775bfaed1b9231b5ba5022f
kevin <kpsanpedro@myseneca.ca>
2022-07-31 22:08:48 -0400

    update for lab9

--- src/model/data/index.js
+++ src/model/data/index.js
@@ -1,1 +1,8 @@
-module.exports = require('./memory');
+// src/model/data/index.js
+
+// If the environment sets an AWS Region, we'll use AWS backend
+// services (S3, DynamoDB); otherwise, we'll use an in-memory db.
+// eslint-disable-next-line no-undef
+module.exports = process.env.AWS_REGION ? require('./aws') : require('./memory');
+
+//module.exports = require('./memory');
