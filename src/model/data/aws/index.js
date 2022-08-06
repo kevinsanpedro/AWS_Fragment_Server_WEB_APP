33a27215d75780e2a0e16669cb7fbd103cdc5997
kevin <kpsanpedro@myseneca.ca>
2022-08-06 12:27:06 -0400

    update lab 9

--- src/model/data/aws/index.js
+++ src/model/data/aws/index.js
@@ -54,7 +54,7 @@ async function writeFragmentData(ownerId, id, data) {
     // If anything goes wrong, log enough info that we can debug
     const { Bucket, Key } = params;
     logger.error({ err, Bucket, Key }, 'Error uploading fragment data to S3');
-    throw new Error('unable to upload fragment data');
+    throw new Error({ err, Bucket, Key }, 'unable to upload fragment data');
   }
 }
 
