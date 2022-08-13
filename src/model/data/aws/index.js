8ff8781016113251b163f25c020a0ab5a630f658
kevin <kpsanpedro@myseneca.ca>
2022-08-13 06:22:11 -0400

    update lab 10

--- src/model/data/aws/index.js
+++ src/model/data/aws/index.js
@@ -115,7 +115,9 @@ async function readFragmentData(ownerId, id) {
     // Get the object from the Amazon S3 bucket. It is returned as a ReadableStream.
     const data = await s3Client.send(command);
     // Convert the ReadableStream to a Buffer
-    return streamToBuffer(data.Body);
+
+    //return streamToBuffer(data.Body);
+    return data;
   } catch (err) {
     const { Bucket, Key } = params;
     logger.error({ err, Bucket, Key }, 'Error streaming fragment data from S3');
